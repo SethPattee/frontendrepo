f9b8aa7e05795e64856c034e26b1e12f44dfae78
Seth Pattee <seth.pattee@students.snow.edu>
2024-08-30 10:15:59 -0600

    message

---
+++ src/components/Emily.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './LoveMessage.css'; // We'll add some custom styles here
+
+const LoveMessage = () => {
+  return (
+    <div className="love-message-container">
+      <h1 className="love-message">
+        I Love You Emily!!
+        <span className="heart">❤️</span>
+      </h1>
+    </div>
+  );
+};
+
+export default LoveMessage;
